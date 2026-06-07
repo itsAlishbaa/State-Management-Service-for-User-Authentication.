dce3a398acefe2cb90c956c584b8b68452de2fd3
itsAishbaa <aalishbaa3@gmail.com>
2026-06-07 11:43:09 -0700

    adding code

--- Components/Pages/AuthenticationStateService.cs
+++ Components/Pages/AuthenticationStateService.cs
@@ -1,6 +1,28 @@
-﻿namespace Q5.Components.Pages
+﻿using System;
+
+public class AuthenticationStateService
 {
-    public class AuthenticationStateService
+    public bool IsAuthenticated { get; private set; } = false;
+
+    public event Action? OnStateChanged;
+
+    public void LogIn()
     {
+        if (!IsAuthenticated)
+        {
+            IsAuthenticated = true;
+            NotifyStateChanged();
+        }
     }
+
+    public void LogOut()
+    {
+        if (IsAuthenticated)
+        {
+            IsAuthenticated = false;
+            NotifyStateChanged();
+        }
+    }
+
+    private void NotifyStateChanged() => OnStateChanged?.Invoke();
 }
